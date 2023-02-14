860fea87c23dfe0efedb260ef7995c2d6a47d1e8
Luigi Mazzocchi <tun50126@temple.edu>
2023-02-14 13:24:42 -0500

    Ran fine and worked when I opened the project from the master fork, but after I made my own fork and opened a new project with that URL, my build is not good and I am unable to run to make sure this project works.

--- app/src/main/java/edu/temple/activity4/MainActivity.kt
+++ app/src/main/java/edu/temple/activity4/MainActivity.kt
@@ -2,7 +2,11 @@ package edu.temple.activity4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -14,14 +18,42 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        textSizeSelector = findViewById(R.id.textSizeSelectorRecyclerView)
+        textSizeDisplay = findViewById(R.id.textSizeDisplayTextView)
+
         // Trying to create array of integers that are multiples of 5
         // Verify correctness by examining array values.
         val textSizes = Array(20){(it + 1) * 5}
+
+        for(i in 0 until textSizes.size)
+            Log.d("Array Values", textSizes[i].toString())
+
+        textSizeSelector.adapter = TextSizeAdapter(textSizes)
+        textSizeSelector.layoutManager = LinearLayoutManager(this)
+
     }
 }
 
 
 /* Convert to RecyclerView.Adapter */
-class TextSizeAdapter {
+class TextSizeAdapter(_textSizes : Array<Int>) : RecyclerView.Adapter<TextSizeAdapter.TextSizeViewHolder>() {
 
+    private val textSizes = _textSizes
+
+    class TextSizeViewHolder(view : TextView) : RecyclerView.ViewHolder (view) {
+        val textView = view
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextSizeViewHolder {
+        return TextSizeViewHolder (TextView(parent.context).apply { setPadding(5, 20, 0, 20) })
+    }
+
+    override fun onBindViewHolder(holder: TextSizeAdapter.TextSizeViewHolder, position: Int) {
+        holder.textView.text = textSizes[position].toString()
+        holder.textView.textSize = textSizes[position].toFloat()
+    }
+
+    override fun getItemCount(): Int {
+        return textSizes.size
+    }
 }
